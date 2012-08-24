84e9426239fedd4074303e6121d000019ee8bab5
Coleman Watts <colemanw@woolman.org>
2012-08-23 18:11:46 -0700

    Add search by semester

--- CRM/Contact/Form/Search/Custom/relationsToWoolman.php
+++ CRM/Contact/Form/Search/Custom/relationsToWoolman.php
@@ -19,15 +19,30 @@ class CRM_Contact_Form_Search_Custom_relationsToWoolman
           'current_students' => 'Current Students',
           'current_parents' => 'Parents of Current Students',
           'current_family' => 'Family of Current Students',
-          'jws_alum' => 'JWS Alum',
-          'ws_alum' => 'WS Alum',
-          'jws_parents' => 'Parents of JWS Alum',
+          'sem_students' => 'Students of a Particular Semester',
+          'sem_parents' => 'Parents of a Particular Semester',
+          'sem_family' => 'Family of a Particular Semester',
+          'ws_alum' => 'All WS Alum',
           'ws_parents' => 'Parents of WS Alum',
-          'jws_family' => 'Family of JWS Alum',
           'ws_family' => 'Family of WS Alum',
+          'jws_alum' => 'JWS Alum',
+          'jws_parents' => 'Parents of JWS Alum',
+          'jws_family' => 'Family of JWS Alum',
         );
         $form->addElement('select', 'woolman', 'Relationship To Woolman:', $woolman);
-        $elements = array('woolman');
+        
+        $this_year = date('Y');
+        for ($y = 2004; $y <= $this_year; ++$y) {
+          for ($s = 1; $s < 7; $s += 5) {
+            if ($y == $this_year && $s == 1 && date('n') < 6) {
+              continue;
+            }
+            $label = $y . ($s == 1 ? ' Spring' : ' Fall');
+            $sems[$y . '_' . $s] = $label;
+          }
+        }
+        $form->addElement('select', 'semester', 'Specify Semester:', $sems);
+        $elements = array('woolman', 'semester');
 
         $status = woolman_get_civi_options(114);
         foreach ($status as $key => $label) {
@@ -64,7 +79,7 @@ contact_a.sort_name    as sort_name
         $params = $exclude = array( );
         $where = 'SELECT contact_id_a FROM civicrm_relationship WHERE contact_id_b = 243 AND relationship_type_id = 10 AND case_id IS NULL';
 
-        list( $program, $people ) = split('_', $this->_formValues['woolman']);
+        list($program, $people) = split('_', $this->_formValues['woolman']);
 
         if ($program == 'current') {
           $where .= ' AND start_date < DATE_ADD(CURDATE(), interval 1 month) AND end_date >= DATE_SUB(CURDATE(), interval 1 week)';
@@ -73,7 +88,13 @@ contact_a.sort_name    as sort_name
           $where .= ' AND YEAR(start_date) > 2003 AND end_date <= CURDATE()';
         }
         elseif ($program == 'jws') {
-          $where .= ' AND (YEAR(start_date) < 2003 OR YEAR(end_date) < 2003 OR (start_date IS NULL AND end_date IS NULL AND is_active = 0))';
+          $where .= ' AND (YEAR(start_date) < 2004 OR YEAR(end_date) < 2004 OR (start_date IS NULL AND end_date IS NULL AND is_active = 0))';
+        }
+        elseif ($program == 'sem') {
+          list($year, $month) = split('_', $this->_formValues['semester']);
+          $start = $year . "0{$month}01";
+          $end = $year . ($month == 1 ? '0701' : '1231');
+          $where .= " AND start_date >= $start AND end_date <= $end";
         }
 
         foreach (woolman_get_civi_options(114) as $key => $label) {
@@ -97,7 +118,7 @@ contact_a.sort_name    as sort_name
     }
 
     function templateFile( ) {
-        return 'CRM/Contact/Form/Search/Custom.tpl';
+        return 'CRM/Contact/Form/Search/Custom/relationsToWoolman.tpl';
     }
 
     function setTitle( $title ) {
